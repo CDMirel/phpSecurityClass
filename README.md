6763f8a23568368f4a24ed06333124c3b7d8496e
PTKDev <ptkdev@gmail.com>
2012-12-20 22:50:06 +0100

    [release] version v0.1

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-phpSecurityClass
-================
+#phpSecurityClass v0.1
 
 Php regular expression for all html-input tag
+
+## How to Install
+
+1) Copy src/phpSecurityClass.php file on your website
+
+2) Add class after `<body>`:
+```php
+<php 
+ include("phpSecurityClass.php"); 
+ $security = new phpSecurityClass();
+?>
+```
+
+2) Choose the appropriate function:
+```php
+if($security->name("John Titor"){
+	echo "Correct Input";
+}else{
+	echo "Error";
+}
+?>
+```
+
+## Support
+
+You can find us on this web source:
+
+WebSite: http://www.ptkdev.it/
+
+Source: https://github.com/PTKDev/
+
+IRC: chat.freenode.net at #ptkdev
---
+++ src/phpSecurityClass.php
@@ -0,0 +1,120 @@
+<?php
+/*
+ * phpSecurityClass v0.1
+ *   Copyright (C) 2012  PTKDev
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+class phpSecurityClass
+{
+
+	public function code($input)
+	{
+		if (preg_match("/^([A-Za-z0-9_\-\.])+$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function nickname($input)
+	{
+		if (preg_match("/^([A-Za-z0-9_\-\.])+$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function name($input)
+	{
+		if (preg_match("/^([A-Za-z0-9_\-\.])+$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function surname($input)
+	{
+		if (preg_match("/^([A-Za-z0-9_\-\'\.])+$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function email($input)
+	{
+		if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function emaildns($input)
+	{
+		if (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $input)){
+		  $domain = explode("@", $input);
+		  if (checkdnsrr($domain[1], "MX"))
+				return 1;
+		}
+		
+		return 0;
+	}
+	
+	public function password($input)
+	{	
+		return $input = sha1(md5($input);
+	}
+	
+	public function url($input)
+	{
+		if (preg_match("/^(http:\/\/|https:\/\/)+([a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@\:]*)*$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function ftp($input)
+	{
+		if (preg_match("/^(ftp.|ftps.)+([a-zA-Z0-9\.\,\?\'\\/\+&amp;%\$#\=~_\-@\:]*)*$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function phone($input)
+	{
+		if (preg_match("/^\d{3,10}$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function cap($input)
+	{
+		if (preg_match("/^\d{5}$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+	public function num($input)
+	{
+		if (preg_match("/^\d{1,15}$/", $input))
+			return 1;	
+		
+		return 0;
+	}
+	
+}
+?>
